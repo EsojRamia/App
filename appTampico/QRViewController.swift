d78c9c9b13aff1ecd5feeb74be8c69b861851dcb
ITESM CAMPUS TAMPICO <Administrador@graphic-design25.local>
2016-10-02 16:33:26 -0500

    Esta es la nueva actualización

---
+++ appTampico/QRViewController.swift
@@ -0,0 +1,100 @@
+//
+//  QRViewController.swift
+//  appTampico
+//
+//  Created by ITESM CAMPUS TAMPICO on 02/10/16.
+//  Copyright © 2016 ITESM CAMPUS TAMPICO. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            captureSession?.startRunning()
+            view.bringSubviewToFront(infoLabel)
+            qrCodeFrameView = UIView()
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.redColor().CGColor
+                qrCodeFrameView.layer.borderWidth = 3
+                view.addSubview(qrCodeFrameView)
+                view.bringSubviewToFront(qrCodeFrameView)
+            }
+        } catch {
+            print(error)
+            return
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRectZero
+            infoLabel.text = "QR Code is not detected"
+            return
+        }
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj)
+        qrCodeFrameView?.frame = barCodeObject!.bounds
+        if metadataObj.stringValue != nil {
+            infoLabel.text = metadataObj.stringValue
+        }
+        launchApp(metadataObj.stringValue)
+    }
+    func launchApp(decodedURL: String) {
+        if presentedViewController != nil {
+            return
+        }
+        let alertPrompt = UIAlertController(title: "Open App", message: "You're going to open \(decodedURL)", preferredStyle: .ActionSheet)
+        let confirmAction = UIAlertAction(title: "Confirm", style: .Default, handler: { (action) -> Void in
+            if let url = NSURL(string: decodedURL) {
+                if UIApplication.sharedApplication().canOpenURL(url) {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertPrompt.addAction(confirmAction)
+        alertPrompt.addAction(cancelAction)
+        presentViewController(alertPrompt, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
